4934c564f49c008689d58825b17055e5305f480e
Kseniya Tetercheva <snegurka.tkd@gmail.com>
2024-06-05 13:17:53 +0300

    add graphic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from random import randint
 
-# from graphic_arts.start_game_banner import run_screensaver
+from graphic_arts.start_game_banner import run_screensaver
 
 DEFAULT_ATTACK = 5
 DEFAULT_DEFENCE = 10
@@ -105,7 +105,7 @@ def choice_char_class(char_name: str) -> Character:
 
 
 if __name__ == '__main__':
-   # run_screensaver()
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
